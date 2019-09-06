f8b5e48a35efb36afe44907bd4b59a6f27e75c13
Stefan Klas <sk15962@bristol.ac.uk>
2019-09-06 21:31:33 +0100

    test

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,4 +1,8 @@
-public class void main(String[] args){
-  System.out.println("test");
-  return 1;
+public class MainClass {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
 }
