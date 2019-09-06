c0a1263bc0a76e88d651430e59d4793a071a2565
Stefan Klas <sk15962@bristol.ac.uk>
2019-09-06 21:37:37 +0100

    test

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,6 +3,7 @@ public class MainClass {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+          System.out.println("Hello, World");
     }
 
 }
