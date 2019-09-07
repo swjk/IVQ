675424d1a595d07c66dd8177e396252fbd475af6
Stefan Klas <sk15962@bristol.ac.uk>
2019-09-07 13:00:40 +0100

    failed test

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test;
 public class MainClassTest{
   @Test
   public void mainTest(){
-    assertEquals(0, 0, "Must be equal");
+    assertEquals(0, 1, "Must be equal");
   }
 }
