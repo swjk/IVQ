be1e120e371095fae25e5a7f0743b8a7b6d897ea
Stefan Klas <sk15962@bristol.ac.uk>
2019-09-07 12:10:50 +0100

    add test

---
+++ src/test/java/MainClassTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainClassTest{
+  @Test
+  public void mainTest(){
+    assertEquals(0, 0, "Must be equal");
+  }
+}
